c27a3756b833f71e43c4f9b42a0ea397c0f29dc0
hollydaly02 <hollydaly123@gmail.com>
2024-10-18 21:50:11 +0100

    serach

--- src/main/java/com/example/lucene/CranfieldParserIndexer.java
+++ src/main/java/com/example/lucene/CranfieldParserIndexer.java
@@ -11,7 +11,6 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
